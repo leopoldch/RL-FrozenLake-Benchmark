3550f20afac1252b5225dcc66be714212aaf8f7d
Léo <leopold@lchappuis.fr>
2026-03-21 17:10:58 -0400

    fix(env): fix corridor success_rate

--- environments/corridor.py
+++ environments/corridor.py
@@ -12,5 +12,5 @@ class Corridor:
         ]
         self.map_name = None
         self.is_slippery = True
-        self.success_rate = 0.30
+        self.success_rate = 0.70
         self.reward_schedule = (1, 0, 0)
